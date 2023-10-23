1d2bf71ae1486ff09bd3634953bb45cd74dd6d51
Danh <handoiditu1508@gmail.com>
2023-10-23 18:05:18 +0700

    number basics

---
+++ tutorial_2/numbers.py
@@ -0,0 +1,23 @@
+print(2 + 2)  # 4
+print(50 - 5 * 6)  # 20
+print((50 - 5 * 6) / 4)  # 5.0
+print(8 / 5)  # 1.6
+
+# classic division returns a float
+print(17 / 3)  # 5.666666666666667
+# floor division discards the fractional part
+print(17 // 3)  # 5
+# the % operator returns the remainder of the division
+print(17 % 3)  # 2
+# floored quotient * divisor + remainder
+print(5 * 3 + 2)  # 17
+
+# 5 squared
+print(5**2)  # 25
+# 2 to the power of 7
+print(2**7)  # 128
+
+# assignment
+width = 20
+height = 5 * 9
+print(width * height)  # 900
